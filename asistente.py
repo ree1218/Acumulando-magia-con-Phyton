032521c688e23392654cb5def5e740f6af7acd96
Regina Navarro Álvarez <145035896+ree1218@users.noreply.github.com>
2023-11-14 09:39:54 -0600

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -1,12 +1,1 @@
-def detecta_alexa(texto):
-    palabras = texto.split()
-    return palabras.count("Alexa")
 
-texto = input("Introduce un texto: ")
-
-numero_alexas = detecta_alexa(texto)
-if numero_alexas == 1:
-   print("¿Como puedo ayudarte?")
-    
-elif numero_alexas > 1:
-    print("Tranquilo, solo di mi nombre una vez")
