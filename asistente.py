466c15502b2160f56a2590a6eae1f87bad421377
Regina Navarro Álvarez <145035896+ree1218@users.noreply.github.com>
2023-11-14 20:24:38 -0600

    Update asistente.py

--- asistente.py
+++ asistente.py
@@ -1,1 +1,8 @@
+asistente = input("Escribe el nombre: ")
+x = asistente.split()
+repeticion_alexa = x.count("Alexa")
 
+if repeticion_alexa == 1:
+    print("Dime cómo puedo ayudarte?")
+elif repeticion_alexa > 1:
+    print("¡Tranquilo, solo di mi nombre una vez!")
